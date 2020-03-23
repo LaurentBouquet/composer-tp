0908632786d7ae455892014d05a6e9a379a4bcae
Laurent Bouquet <laurent.bouquet@joliciel.fr>
2020-03-23 14:52:24 +0100

    First commit

--- src/index.php
+++ src/index.php
@@ -1,12 +1,12 @@
 <?php
 require_once '../vendor/autoload.php';
 
-use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 $logger = new Logger('main');
 $logger->pushHandler(new StreamHandler(__DIR__ . '/../log/app.log', Logger::DEBUG));
 
 $logger->info('First message');
 
-print('Le message est enregistré dans le fichier ' . __DIR__ . '/../log/app.log');
+print("OK");
